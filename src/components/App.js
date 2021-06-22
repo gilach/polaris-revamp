71adadb15f4cdcc5ed299f669e2928f2e36573e5
Simone Busoli <simone.busoli@gmail.com>
2021-06-22 12:55:56 +0200

    chore: initial setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+import colors from '../theme/colors'
 
 export default function App() {
   return (
@@ -8,14 +10,14 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: colors.white,
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
